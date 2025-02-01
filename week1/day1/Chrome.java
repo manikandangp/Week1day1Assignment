c2f5a721b558510be91fc0c2c9f1e17a35330bb8
manikandangp <100836873+manikandangp@users.noreply.github.com>
2025-02-01 20:24:42 +0530

    Add files via upload

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		float version=91.0f;
+		String developer="Google";
+		boolean beta=false;
+		int release=2008;
+		char shortcutKey='c';
+		
+		
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(beta);
+		System.out.println(release);
+		System.out.println(shortcutKey);
+		
+		
+
+	}
+
+}
