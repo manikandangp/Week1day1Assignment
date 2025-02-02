72b3951f0c000abb51d6c919e75516bbb4bad83e
Manikandan <100836873+manikandangp@users.noreply.github.com>
2025-02-02 12:09:57 +0530

    Add files via upload

---
+++ week1/day2/FindOddNumbers.java
@@ -0,0 +1,27 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=10;i++)
+		{
+			
+			if(i%2==1) {
+				
+				System.out.println(i+"is odd number");
+			}
+			else {
+				System.out.println(i+"is Even number");
+			}
+			}
+		}
+		
+		
+		
+		
+		
+	}
+
+
